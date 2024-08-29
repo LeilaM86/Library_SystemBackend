fa3e4788c8926f36b056ee33aacd6340c07b6e76
Leila <leilamola86@gmail.com>
2024-08-30 00:13:50 +0200

    fixed cors

--- index.ts
+++ index.ts
@@ -7,21 +7,13 @@ import libraryItems from "./routes/libraryItems";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:4173",
-      "https://library-system-frontend-4iuq.onrender.com",
-    ],
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api/categories", categories);
 app.use("/api/library-items", libraryItems);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 
-const PORT = process.env.PORT || 7577;
+const PORT = process.env.PORT || 7578;
 
-app.listen(PORT, () => console.log("Listening on port " + PORT));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
